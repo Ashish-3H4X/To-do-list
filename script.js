0f2777e6a3957225cfe9a9e35b183e9b94585a17
Mr Rahul <ashish227087@gmail.com>
2025-03-19 23:02:15 +0530

    added delete functionality

--- script.js
+++ script.js
@@ -12,24 +12,50 @@ body.appendChild(allTasksContainer);
 const createTasks = () => {
 
     allTasksContainer.innerHTML = "";
-  allTasks.forEach((item,index)=>{
-    
-    const task = document.createElement("div");
-    task.classList.add("task");
+    allTasks.forEach((item, index) => {
 
-    const title = document.createElement("p");
-    title.classList.add("title");
-    title.textContent = item.title;
+        const task = document.createElement("div");
+        task.classList.add("task");
 
-    const desc = document.createElement("p");
-    desc.classList.add("desc");
-    desc.textContent = item.description;
+        const title = document.createElement("p");
+        title.classList.add("title");
+        title.textContent = item.title;
 
-    task.appendChild(title);
-    task.appendChild(desc);
-    allTasksContainer.appendChild(task);
-  })
+        const desc = document.createElement("p");
+        desc.classList.add("desc");
+        desc.textContent = item.description;
+
+        const controls = document.createElement("div");
+        controls.classList.add("controls");
+
+        const deleteBtn = document.createElement("i");
+        deleteBtn.classList.add("bi","bi-trash","delete");
+
+        deleteBtn.addEventListener("click",()=>{
+           deleteTask(index)
+        });
+
+        const completeBtn = document.createElement("i");
+        completeBtn.classList.add("bi","bi-check-square")
+
+        const updateBtn = document.createElement("i");
+        updateBtn.classList.add("bi","bi-pencil-square");
+       
+        controls.appendChild(updateBtn);
+        controls.appendChild(deleteBtn);
+        controls.appendChild(completeBtn);
+        task.appendChild(controls);
+        task.appendChild(title);
+        task.appendChild(desc);
+        allTasksContainer.appendChild(task);
+    })
 };
+
+const deleteTask = (index)=>{
+    allTasks.splice(index,1);
+    localStorage.setItem("tasks",JSON.stringify(allTasks));
+    createTasks();
+}
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -45,7 +71,7 @@ form.addEventListener("submit", (e) => {
     titleElem.value = "";
     descElem.value = "";
 
-    localStorage.setItem("tasks",JSON.stringify(allTasks));
+    localStorage.setItem("tasks", JSON.stringify(allTasks));
 });
 
 createTasks();
